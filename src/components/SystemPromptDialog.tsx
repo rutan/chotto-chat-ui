277c42ced39cbd5deca68949c684dcda049a24a1
rutan <ru_shalm@hazimu.com>
2024-09-02 23:51:29 +0900

    fix callback

--- src/components/SystemPromptDialog.tsx
+++ src/components/SystemPromptDialog.tsx
@@ -27,7 +27,7 @@ export const SystemPromptDialog = ({
   const handleCancel = useCallback(() => {
     setSystemPrompt(initSystemPrompt);
     onCancel();
-  }, [onCancel]);
+  }, [initSystemPrompt, onCancel]);
 
   useEffect(() => {
     setSystemPrompt(initSystemPrompt);
